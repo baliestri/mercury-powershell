28ba319167492548909e94ed4221a649ab6e25e3
Bruno Sales <me@baliestri.dev>
2024-09-30 15:14:25 -0300

    feat (storage): exclude code coverage

--- source/core/Mercury.PowerShell.Storage/Options/ConfigureStorageOptions.cs
+++ source/core/Mercury.PowerShell.Storage/Options/ConfigureStorageOptions.cs
@@ -1,11 +1,13 @@
 // Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
 // See the LICENSE file in the repository root for full license text.
 
+using System.Diagnostics.CodeAnalysis;
 using Mercury.PowerShell.Storage.Options.Abstractions;
 using SQLite;
 
 namespace Mercury.PowerShell.Storage.Options;
 
+[ExcludeFromCodeCoverage]
 internal sealed class ConfigureStorageOptions : IConfigureStorageName, IConfigureStorageFlags {
   private SQLiteOpenFlags _flags = SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache;
   private string _name = string.Empty;
