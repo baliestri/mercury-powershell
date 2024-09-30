4b8f8a0ca2d0c4fbc497d7759b5d15bb097d1712
Bruno Sales <me@baliestri.dev>
2024-09-30 12:04:43 -0300

    feat (storage): add internal methods

--- source/core/Mercury.PowerShell.Storage/Options/StorageOptions.cs
+++ source/core/Mercury.PowerShell.Storage/Options/StorageOptions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
 // See the LICENSE file in the repository root for full license text.
 
+using System.Diagnostics.CodeAnalysis;
 using Mercury.PowerShell.Storage.Options.Abstractions;
 using SQLite;
 
@@ -9,9 +10,11 @@ namespace Mercury.PowerShell.Storage.Options;
 /// <summary>
 ///   Options for storage.
 /// </summary>
+[ExcludeFromCodeCoverage]
 public sealed class StorageOptions : IStorageOptions {
   internal static readonly StorageOptions Empty = new() {
-    Name = string.Empty
+    Name = string.Empty,
+    OpenFlags = SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache
   };
 
   /// <inheritdoc />
@@ -22,4 +25,17 @@ public sealed class StorageOptions : IStorageOptions {
 
   /// <inheritdoc />
   public IEnumerable<Type> Tables { get; set; } = [];
+
+  internal static IStorageOptions WithName(string name)
+    => new StorageOptions {
+      Name = name,
+      OpenFlags = SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache
+    };
+
+  internal static IStorageOptions WithNameAndTables(string name, IEnumerable<Type> tables)
+    => new StorageOptions {
+      Name = name,
+      OpenFlags = SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache,
+      Tables = tables
+    };
 }
