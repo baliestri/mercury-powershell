35dbf8c288be3e15913e7ab66a6cf45ccbd1d43a
Bruno Sales <me@baliestri.dev>
2024-09-30 15:14:40 -0300

    test (dependency-injection): exclude code coverage

--- tests/core/Mercury.PowerShell.DependencyInjection.UnitTests/Services/TestServiceNotInjected.cs
+++ tests/core/Mercury.PowerShell.DependencyInjection.UnitTests/Services/TestServiceNotInjected.cs
@@ -1,8 +1,11 @@
 // Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
 // See the LICENSE file in the repository root for full license text.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Mercury.PowerShell.DependencyInjection.UnitTests.Services;
 
+[ExcludeFromCodeCoverage]
 public sealed class TestServiceNotInjected {
   public void DoSomething()
     => Console.WriteLine("I'm doing something!");
