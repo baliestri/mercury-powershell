e27a0585ef3a552dbd258e29c60f38462b27897b
Bruno Sales <me@baliestri.dev>
2024-09-30 11:11:29 -0300

    feat (storage): add base entity object

---
+++ source/core/Mercury.PowerShell.Storage/Entity.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+
+namespace Mercury.PowerShell.Storage;
+
+/// <summary>
+///   Represents an entity.
+/// </summary>
+public abstract class Entity {
+  /// <summary>
+  ///   The entity identifier.
+  /// </summary>
+  [PrimaryKey]
+  public Ulid Id { get; init; } = Ulid.NewUlid();
+
+  /// <summary>
+  ///   The entity creation date.
+  /// </summary>
+  public DateTimeOffset CreatedAt { get; init; } = DateTimeOffset.UtcNow;
+
+  /// <summary>
+  ///   The entity update date.
+  /// </summary>
+  public DateTimeOffset? UpdatedAt { get; set; }
+}
