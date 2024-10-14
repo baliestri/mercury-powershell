67df202e3986122a69c9746b903b3e91e3e3f9d9
Bruno Sales <me@baliestri.dev>
2024-10-14 14:14:37 -0300

    feat (module/hooks): add cmdlet to unregister proxy hooks

---
+++ source/modules/Mercury.PowerShell.Hooks/Cmdlets/UnregisterProxyHookCmdlet.cs
@@ -0,0 +1,63 @@
+// Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Management.Automation;
+using Mercury.PowerShell.Hooks.ArgumentCompleters.Attributes;
+using Mercury.PowerShell.Hooks.ComplexTypes;
+using Mercury.PowerShell.Hooks.Enums;
+
+namespace Mercury.PowerShell.Hooks.Cmdlets;
+
+/// <summary>
+///   Cmdlet to unregister a proxy hook.
+/// </summary>
+[OutputType(typeof(HookItem))]
+[Cmdlet(VerbsLifecycle.Unregister, "ProxyHook")]
+public sealed class UnregisterProxyHookCmdlet : PSCmdlet {
+  /// <summary>
+  ///   The type of the hook.
+  /// </summary>
+  [Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, Mandatory = true)]
+  public required HookType Type { get; init; }
+
+  /// <summary>
+  ///   The unique identifier of the hook.
+  /// </summary>
+  [Parameter(Position = 1, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, Mandatory = true)]
+  [HookStoreIdentifierCompleter]
+  public required string Identifier { get; init; }
+
+  /// <summary>
+  ///   Passes an object representing the hook to the pipeline. By default, this cmdlet does not generate any output.
+  /// </summary>
+  [Parameter]
+  public SwitchParameter PassThru { get; init; }
+
+  /// <inheritdoc />
+  protected override void ProcessRecord() {
+    var hookVariableName = Type switch {
+      HookType.ChangeWorkingDirectory => HookVariableNames.ChangeWorkingDirectory,
+      HookType.PrePrompt => HookVariableNames.PrePrompt,
+      var _ => throw new ArgumentOutOfRangeException(nameof(Type), Type, "The hook type is not valid.")
+    };
+
+    var hookVariable = SessionState.PSVariable.Get(hookVariableName);
+
+    if (hookVariable?.Value is not HookStore hookStore) {
+      return;
+    }
+
+    var item = hookStore.Items.FirstOrDefault(item => item.Identifier == Identifier);
+
+    if (!hookStore.Items.Remove(item) &&
+        PassThru.IsPresent) {
+      WriteObject($"The hook with identifier '{Identifier}' was not found in the '{Type}' hook store.");
+
+      return;
+    }
+
+    if (PassThru.IsPresent) {
+      WriteObject(item);
+    }
+  }
+}
